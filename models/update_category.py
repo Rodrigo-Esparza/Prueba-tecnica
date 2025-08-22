418dbe8822909dd1b9573d9d2744a11d4fbbef83
Rodrigo-Esparza <72179596+Rodrigo-Esparza@users.noreply.github.com>
2025-08-22 11:49:10 -0600

    Update update_category.py

--- models/update_category.py
+++ models/update_category.py
@@ -1,1 +1,10 @@
+from odoo import fiels, models
 
+Class update_category(model.models)
+ _inherit = "updatecategory"
+
+   update_category = fiels. Many2many(
+     "purchase.category",
+       string ="Se ha actualizado tus categorías con exito"
+       help = "No se ha logrado actualizar favor de validar error"
+) 
